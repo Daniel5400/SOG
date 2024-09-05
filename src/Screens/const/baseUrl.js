2068c56d62a48b1bf766248a58446b80e090ecb7
Daniel5400 <obinnadaniel5400@gmail.com>
2024-09-05 12:50:02 +0100

    url

--- src/Screens/const/baseUrl.js
+++ src/Screens/const/baseUrl.js
@@ -1,5 +1,7 @@
 
 
-let  baseURL = 'https://backendcar-xmub.onrender.com/api/v1/'
+ let  baseURL = 'https://backendcar-xmub.onrender.com/api/v1/'
+//let  baseURL = 'http://localhost:3000/api/v1/'
+
 
 export default baseURL;
